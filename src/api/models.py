ac14781bdc3514501968fa0534fd3c14ff379eb8
Javad <javad.zahedi2001@gmail.com>
2022-01-03 18:24:48 +0330

    add api models

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class App(models.Model):
+    name = models.CharField(max_length=128)
+    image = models.URLField(max_length=512)
+    command = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
+
+
+class KeyValue(models.Model):
+    key = models.CharField(max_length=128)
+    value = models.CharField(max_length=128)
+    app = models.ForeignKey(App)
+
+    def __str__(self):
+        return self.key
