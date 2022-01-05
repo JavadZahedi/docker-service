2f80289622d13ba1085e0aa3c07c572f216513fb
Javad <javad.zahedi2001@gmail.com>
2022-01-05 20:02:58 +0330

    register Container to admin panel

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import App, EnvVar
+from .models import App, Container, EnvVar
 
 
 admin.site.register(App)
 admin.site.register(EnvVar)
+admin.site.register(Container)
